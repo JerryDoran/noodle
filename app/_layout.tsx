b39ab146704006311ce63facef7f404d5aeba197
Jerome Doran <disneyfan9184@gmail.com>
2025-10-18 15:03:57 -0400

    feat: remove reset-project script and add Tailwind CSS configuration
    - Deleted the reset-project.js script that was used to reset the project state.
    - Added a new tailwind.config.js file to configure Tailwind CSS with custom colors and font families.
    - Updated tsconfig.json to include nativewind-env.d.ts for TypeScript support with NativeWind.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import './global.css';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [fontsLoaded, error] = useFonts({
+    'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+    'Quicksand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+    'Quicksand-Regular': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'Quicksand-SemiBold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
+  });
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  // const [fonts] = useFonts(map:{
+  //   "Quicksand-Bold": require(id:"../assets/fonts/Quicksand-Bold.ttf"),
+  //   "Quicksand-Light": require(id:"../assets/fonts/Quicksand-Light.ttf"),
+  //   "Quicksand-Medium": require(id:"../assets/fonts/Quicksand-Medium.ttf"),
+  //   "Quicksand-Regular": require(id:"../assets/fonts/Quicksand-Regular.ttf"),
+  //   "Quicksand-SemiBold": require(id:"../assets/fonts/Quicksand-SemiBold.ttf"),
+  // });
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
