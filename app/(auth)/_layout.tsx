5e27afa5add862ebb3f8094c4a29bb33b746f638
Jerome Doran <disneyfan9184@gmail.com>
2025-10-26 11:15:06 -0400

    feat: enhance authentication layout with custom components and styles

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,52 @@
+import CustomButton from '@/components/custom-button';
+import CustomInput from '@/components/custom-input';
+import { images } from '@/constants';
 import { Slot } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
 
 export default function AuthLayout() {
   return (
-    <SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView
+        className='bg-white h-full'
+        keyboardShouldPersistTaps='handled'
+      >
+        <View
+          className='w-full relative'
+          style={{ height: Dimensions.get('screen').height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className='size-full rounded-b-lg'
+          />
+          <Image
+            source={images.logo}
+            className='absolute z-10 self-center size-48 -bottom-16'
+          />
+        </View>
+        <View className='mt-10 px-6 space-y-6'>
+          <CustomInput
+            placeholder='Enter your email'
+            value={''}
+            onChangeText={() => {}}
+            label='Email'
+            keyboardType='email-address'
+          />
+          <CustomButton />
+        </View>
+      </ScrollView>
+
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
---
+++ components/custom-button.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export default function CustomButton() {
+  return (
+    <View>
+      <Text>CustomButton</Text>
+    </View>
+  );
+}
