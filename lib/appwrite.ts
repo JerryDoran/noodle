c7fe38dc0aa9088cf7d2750155bcc98583463ebf
Jerome Doran <disneyfan9184@gmail.com>
2025-10-26 11:15:06 -0400

    feat: implement authentication flow with sign-in and sign-up screens, including user state management

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: 'com.maestro.noodle',
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: '68fe160e0010b37e022b',
+  userCollectionId: 'user',
+};
